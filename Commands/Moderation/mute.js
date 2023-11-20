ce681a55d9bd96d5900652c65bdfd6297d9ba70e
Ken <thuonghp4212@gmail.com>
2023-11-20 23:21:10 +0700

    20/11

--- Commands/Moderation/mute.js
+++ Commands/Moderation/mute.js
@@ -1,64 +1,64 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
-const ms = require("ms");
+    const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
+    const ms = require("ms");
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("mute")
-        .setDescription("Câm lặng một người trong máy chủ.")
-        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
-        .addUserOption(option => 
-            option.setName("name")
-                .setDescription("Lựa chọn người muốn câm lặng.")
-                .setRequired(true)
-        )
-        .addStringOption(option => 
-            option.setName("time")
-                .setDescription("Thời gian câm lặng.")
-                .setRequired(true)
-        )
-        .addStringOption(option =>
-            option.setName("reason")
-                .setDescription("Lí do người này bị câm lặng.")
-        ),
-
-    async execute(interaction) {
-        const { guild, options } = interaction;
-
-        const user = options.getUser("name");
-        const member = guild.members.cache.get(user.id);
-        const time = options.getString("time");
-        const convertedTime = ms(time);
-        const reason = options.getString("reason") || "Không có lí do";
-
-        const errEmbed = new EmbedBuilder()
-            .setDescription('Lỗi rồi, hãy thử lại!')
-            .setColor(0xECB2FB);
-
-        const successEmbed = new EmbedBuilder()
-            .setTitle(":white_check_mark: **Muted**")
-            .setDescription(`Staff đã mute cho ${user.tag}.`)
-            .addFields(
-                { name: "Lí do", value: `${reason}`, inline: true },
-                { name: "Thời hạn:", value: `${time}`, inline: true }
+    module.exports = {
+        data: new SlashCommandBuilder()
+            .setName("mute")
+            .setDescription("Câm lặng một người trong máy chủ.")
+            .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+            .addUserOption(option => 
+                option.setName("name")
+                    .setDescription("Lựa chọn người muốn câm lặng.")
+                    .setRequired(true)
             )
-            .setColor(0xECB2FB)
-            .setTimestamp();
+            .addStringOption(option => 
+                option.setName("time")
+                    .setDescription("Thời gian câm lặng.")
+                    .setRequired(true)
+            )
+            .addStringOption(option =>
+                option.setName("reason")
+                    .setDescription("Lí do người này bị câm lặng.")
+            ),
 
-        if (member.roles.highest.position >= interaction.member.roles.highest.position)
-            return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+        async execute(interaction) {
+            const { guild, options } = interaction;
 
-        if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.ModerateMembers))
-            return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+            const user = options.getUser("name");
+            const member = guild.members.cache.get(user.id);
+            const time = options.getString("time");
+            const convertedTime = ms(time);
+            const reason = options.getString("reason") || "Không có lí do";
 
-        if (!convertedTime)
-            return interaction.reply({ embeds: [errEmbed], ephemeral: true });
-            
-        try {
-                await member.timeout(convertedTime, reason);
-    
-                interaction.reply({embeds: [successEmbed], ephemeral: false});
-            } catch (err) {
-                console.log(err);
-            }
-    },
-};
+            const errEmbed = new EmbedBuilder()
+                .setDescription('Lỗi rồi, hãy thử lại!')
+                .setColor(0xECB2FB);
+
+            const successEmbed = new EmbedBuilder()
+                .setTitle(":white_check_mark: **Muted**")
+                .setDescription(`Staff đã mute cho ${user.tag}.`)
+                .addFields(
+                    { name: "Lí do", value: `${reason}`, inline: true },
+                    { name: "Thời hạn:", value: `${time}`, inline: true }
+                )
+                .setColor(0xECB2FB)
+                .setTimestamp();
+
+            if (member.roles.highest.position >= interaction.member.roles.highest.position)
+                return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+
+            if (!interaction.guild.members.me.permissions.has(PermissionFlagsBits.ModerateMembers))
+                return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+
+            if (!convertedTime)
+                return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+                
+            try {
+                    await member.timeout(convertedTime, reason);
+        
+                    interaction.reply({embeds: [successEmbed], ephemeral: false});
+                } catch (err) {
+                    console.log(err);
+                }
+        },
+    };
--- Events/Client/messageCreate.js
+++ Events/Client/messageCreate.js
@@ -3,10 +3,14 @@ module.exports = {
     async execute(message) {
         if (message.author.bot) return;
 
-        if (message.content.includes("https://") || message.content.includes("http://") || message.content.includes("discord.gg")) {
-            message.delete();
+        // Kiểm tra xem người gửi có vai trò phù hợp không
+        const allowedRoleIDs = ["1121667704298946600", "1121764644403560469"]; // Thay thế bằng ID của các vai trò được phép
+        const senderRoleIDs = message.member.roles.cache.map(role => role.id);
 
-            message.channel.send({content: `Này ${message.author}, không được gửi link ở đây nha!`})
+        if (!(allowedRoleIDs.some(roleID => senderRoleIDs.includes(roleID))) &&
+            (message.content.includes("https://") || message.content.includes("http://") || message.content.includes("discord.gg"))) {
+            message.delete();
+            message.channel.send({ content: `Này ${message.author}, bạn không được phép gửi link ở đây!` });
         }
     }
-}
+};
