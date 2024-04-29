699b2e71b72e3404543ced227e9f8880a0cfc13c
Thưởng <tranvietthuong@192.168.0.104>
2024-04-30 01:14:58 +0700

    Xoá debug nối từ tiếng Anh

--- Events/Messages/messageNoichu.js
+++ Events/Messages/messageNoichu.js
@@ -2,15 +2,10 @@ const countingScheme = require("../../Models/Noichu");
 const RankingNoichu = require("../../Models/Rankingnoichu");
 const axios = require("axios");
 
-// Thêm API key và endpoint của WordsAPI
 const apiKey = "c0ac70d184msh79be5892d876e19p161670jsnc017900fd346";
 const apiEndpoint = "https://wordsapiv1.p.rapidapi.com/words/";
 
-// Sử dụng một đối tượng để lưu trữ trạng thái cho từng người chơi và từng từ
 const playerStates = {};
-const usedWords = {};
-
-// Biến mới để lưu ID người chơi cuối cùng đã nhập từ đúng
 let lastPlayerId = null;
 
 module.exports = {
@@ -26,13 +21,11 @@ module.exports = {
 
       if (message.channel.id !== data.Channel) return;
 
-      // Lấy trạng thái của người chơi
       let playerState = playerStates[message.author.id];
 
       if (!playerState) {
-        // Nếu người chơi chưa có trạng thái, tạo mới
         playerState = {
-          enteredWords: [], // Danh sách từ mà người chơi đã nhập
+          enteredWords: [],
         };
         playerStates[message.author.id] = playerState;
       }
@@ -46,16 +39,14 @@ module.exports = {
         return;
       }
 
-      // Kiểm tra từ có hợp lệ trước khi xử lý tiếp
       if (!(await checkValidWord(word))) {
         message.react("<:downvote:1232649248869449738>");
         message.reply(
           `Từ \`${word}\` không hợp lệ! Hãy nhập một từ tiếng Anh hợp lệ.`
         );
-        return; // Dừng xử lý nếu từ không hợp lệ
+        return;
       }
 
-      // Kiểm tra xem từ đã được sử dụng chưa
       if (data.UserWords && data.UserWords.has(word)) {
         message.react("<:downvote:1232649248869449738>");
         message.reply(
@@ -64,7 +55,6 @@ module.exports = {
         return;
       }
 
-      // Cập nhật UserWords trong cơ sở dữ liệu
       if (!data.UserWords) {
         data.UserWords = new Map();
       }
@@ -72,7 +62,6 @@ module.exports = {
       data.UserWords.set(safeWord, true);
       await data.save();
 
-      // Kiểm tra nếu người chơi hiện tại là người chơi cuối cùng đã nhập từ đúng
       if (message.author.id === lastPlayerId) {
         message.reply(
           `Bạn không thể nhập hai từ liên tiếp! Hãy chờ người khác nhập từ.`
@@ -89,7 +78,6 @@ module.exports = {
         return;
       }
 
-      // Kiểm tra xem từ đã được sử dụng chưa
       if (playerState.enteredWords.includes(word)) {
         message.react("<:downvote:1232649248869449738>");
         message.reply(
@@ -99,10 +87,6 @@ module.exports = {
         return;
       }
 
-      // Đánh dấu từ này đã được sử dụng
-      usedWords[word] = true;
-
-      // Cập nhật ID người chơi cuối cùng đã nhập từ đúng
       lastPlayerId = message.author.id;
       this.lastWord = word;
 
@@ -123,31 +107,11 @@ module.exports = {
 };
 
 async function checkValidWord(word) {
-  // Kiểm tra từ có chứa ký tự đặc biệt hay không
   if (/[^a-zA-Z]/.test(word)) {
     return false;
   }
 
   return isValidWordInDictionary(word);
-}
-
-const fs = require("fs");
-
-// Đọc whitelist từ file JSON
-const whitelistPath = "./whitelistnoichu.json";
-let whitelist;
-
-try {
-  const whitelistData = fs.readFileSync(whitelistPath);
-  whitelist = JSON.parse(whitelistData);
-} catch (error) {
-  console.error("Error reading whitelist:", error);
-  whitelist = { whitelist: [] };
-}
-
-async function isValidWordInWhitelist(word) {
-  // Kiểm tra xem từ có trong whitelist hay không
-  return whitelist.whitelist.includes(word);
 }
 
 async function isValidWordInDictionary(word) {
@@ -160,18 +124,13 @@ async function isValidWordInDictionary(word) {
       },
     });
 
-
     const hasDefinition =
       response.data &&
       response.data.results &&
       response.data.results.length > 0 &&
       response.data.results[0].definition;
 
-    // Kiểm tra xem từ có trong whitelist hay không
-    const isInWhitelist = await isValidWordInWhitelist(word);
-
-    // Trả về true nếu có definition hoặc từ nằm trong whitelist
-    return hasDefinition || isInWhitelist;
+    return hasDefinition;
   } catch (error) {
     console.error(error);
     return false;
